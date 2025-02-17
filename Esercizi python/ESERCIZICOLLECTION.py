f4332eb79ab75932637bb14bf3e3d7be94b40e14
Mattpwn <argenti.m@hotmail.com>
2025-02-17 12:51:02 +0100

    esercizi collection

---
+++ Esercizi python/ESERCIZICOLLECTION.py
@@ -0,0 +1,135 @@
+
+# ESERCIZIO 2.3
+
+# name: str = "Giovanni"
+
+# print("Hello Giovanni, would u like to learn some Python today?")
+
+# # ESERCIZIO 2.4
+
+# name: str = "Giovanni"
+
+# print(name.upper())
+# print(name.title())
+# print(name.lower())
+
+# ESERCIZIO 2.5
+
+# famous_person: str = "Io"
+# print(f"{famous_person} un giorno dissi: \"Si vive una volta sola, ma se lo fai bene, una volta è abbastanza \"")
+
+# ESERCIZIO 2.6
+
+# famous_person: str = "Ghandi"
+# message: str = "La paura può servire ma mai la codardia"
+# print(message)
+
+# ESERCIZIO 3.1
+
+# lista_amici: list = ["Francesco", "Riccardo", "Yousef", "Luca"]
+# print(lista_amici[0])
+# print(lista_amici[1])
+# print(lista_amici[2])
+# print(lista_amici[3])
+
+# ESERCIZI 3.2
+
+# lista_amici: list = ["Francesco", "Lorenzo", "Thomas", "Luca"]
+# print("Francesco andiamo a disoneste")
+# print("Lorenzo andiamo a cortigiane")
+# print("Thomas andiamo a professioniste del sesso")
+# print("Luca andiamo a nigeriane")
+
+# # ESERCIZIO 3.3
+
+# cars_list : list = ["audi","ferrari","lamborghini"]
+
+# print(f"{cars_list[0]}, è il miglior marchio tedesco")
+# print(f"{cars_list[1]}, è il miglior marchio italiano")
+# print(f"{cars_list[2]}, è secondo solo a ferrari")
+
+
+#ESERCIZIO 3.4
+
+# famous_person_list : list = ["Totti","Baggio","Del Piero"]
+# print(f"ciao {famous_person_list[0]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[1]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[2]}, vorresti venire a cena?")
+
+# ESERCIZIO 3.5
+
+# famous_person_list : list = ["Totti","Baggio","Del Piero"]
+# famous_person_list.pop(1)
+# famous_person_list.insert(1,"Maldini")
+
+# print(f"ciao {famous_person_list[0]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[1]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[2]}, vorresti venire a cena?")
+
+# ESERCIZIO 3.6 
+
+# famous_person_list : list = ["Totti","Baggio","Del Piero"]
+# famous_person_list.insert(1,"Maldini")
+# famous_person_list.insert(2,"Ronaldinho")
+# famous_person_list.append("Ronaldo")
+
+# print(f"ciao {famous_person_list[0]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[1]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[2]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[3]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[4]}, vorresti venire a cena?")
+# print(f"ciao {famous_person_list[5]}, vorresti venire a cena?")
+
+# ESERCIZIO 3.7
+
+# famous_person_list : list = ["Totti","Baggio","Del Piero"]
+# print(f"ciao, {famous_person_list[1]}, mi dispiace ma non posso invitarti alla cena")
+# famous_person_list.pop(1)
+
+# print(f"ciao {famous_person_list[0]}, la cena è confermata")
+# print(f"ciao {famous_person_list[1]}, la cena è confermata")
+
+# del famous_person_list [0]
+# del famous_person_list [0]
+# print(f"famous_person_list: {famous_person_list}")
+
+# ESERCIZIO 3.8
+
+# list_vacation = ["Giappone","Singapore","Rio de Janeiro","Thailandia","Essaouira"]
+
+# print(list_vacation)
+# print (f"questa è la lista in ordine alfabetico:  {sorted(list_vacation)}")
+# print(list_vacation)
+
+# print(sorted (list_vacation))
+# print(list_vacation)
+
+# list_vacation.reverse()
+# print(list_vacation)
+
+# list_vacation.reverse()
+# print(list_vacation)
+
+# list_vacation.sort()
+# print(list_vacation)
+
+# list_vacation.sort()
+# print(list_vacation)
+
+# ESERCIZIO 3.9
+
+# list : list = ["Hitler","Anna Frank","Benito","Matteotti"]
+
+# print(f"Questi sono gli invitati alla mia cena: {len (list)}")
+
+# ESERCIZIO 3.10
+
+# list_language: list = ["crucco","baguette","taco","pizza"]
+
+# list_language.insert (2,"ronaldo")
+# list_language.append ("forni")
+# list_language.pop (3)
+# del list_language [-2]
+# list_language.sort()
+# list_language.reverse()
+# print(list_language)
