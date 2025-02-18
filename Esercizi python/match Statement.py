e2e09c052c3e1fab0c2e2c04d0885ccca7b4681a
Mattpwn <argenti.m@hotmail.com>
2025-02-18 10:43:20 +0100

    match statement

---
+++ Esercizi python/match Statement.py
@@ -0,0 +1,40 @@
+
+# ESERCIZIO MATCH  STATEMENT
+
+# n: int = int (input("Inserire la posizione: "))
+
+# if n == 1:
+#     print(f" {n}st")
+
+# elif n == 2:
+#     print(f" {n}nd")
+
+# elif n == 3:
+#     print(f"{n}rd")
+
+# else:
+#     n > 3
+#     print(f"{n}th")
+
+# ESEMPIO DI MATCH STATEMENT
+
+# http_status = 200           # | sarebbe la scrittura di OR in codice 
+
+# match http_status:
+
+#     case 200|201:
+#         print("Succes")
+
+#     case 400:
+#         print("Bad Request")
+
+#     case 404:
+#         print("Not Found")
+
+#     case 500|501:
+#         print("Server Error")
+
+#     case _:
+#         print("Unknown")
+
+
