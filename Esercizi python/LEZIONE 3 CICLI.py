8025bc867bda204c2e44d7423110fd119414654f
Mattpwn <argenti.m@hotmail.com>
2025-02-25 18:31:47 +0100

    LEZIONE 3 CICLI

---
+++ Esercizi python/LEZIONE 3 CICLI.py
@@ -0,0 +1,41 @@
+
+# ESERCIZIO 4.1
+
+# lista_pizza: list[str] = ["Diavola", "Pistacchiosa", "Margherita"]
+
+# for i in lista_pizza:
+#     print(i)
+
+# for i in lista_pizza:
+#     print(f"Adoro la {i}")
+
+# print("la pizza è il mio cibo preferito")
+
+# ESERCIZIO 4.2
+
+# lista_animali: list[str] = ["foca", "balena", "narvalo", "pesce rosso"]
+
+# for i in lista_animali:
+#     print(i)
+   
+# for i in lista_animali:
+#     print(f" {i} è un animale acquatico")
+
+# print("il pesce rosso è un ottimo animale domestico")
+
+# ESERCIZIO 4.3
+
+# lista_1 : list = (range(1,21))
+
+# for i in range(1,21):
+#     print(i)
+
+# ESERCIZIO 4.4
+
+lista_1 : list = (range(1,1000001))
+
+for i in range(1,1000001):
+    print(i)
+
+# ESERCIZIO 4.5
+
