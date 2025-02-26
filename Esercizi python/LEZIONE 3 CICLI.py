3700783ab358ea3d64a242a76fea05e5e3652d67
Mattpwn <argenti.m@hotmail.com>
2025-02-26 19:14:03 +0100

    LEZIONE 3 CICLI

--- Esercizi python/LEZIONE 3 CICLI.py
+++ Esercizi python/LEZIONE 3 CICLI.py
@@ -32,10 +32,160 @@
 
 # ESERCIZIO 4.4
 
-lista_1 : list = (range(1,1000001))
+# lista_1 : list = (range(1,1000001))
 
-for i in range(1,1000001):
-    print(i)
+# for i in range(1,1000001):
+#     print(i)
 
 # ESERCIZIO 4.5
+
+# list_number:list[int] = []
+# for i in range(1,1000001):
+#     list_number.append(i)
+# print(min(list_number))
+# print(max(list_number))
+# print(sum(list_number))
+
+# ESERCIZIO 4.6
+
+# list_number : list[int] = []
+
+# for i in range(1,21,2):
+#     print(i)
+
+
+# ESERCIZIO 4.7 
+
+# ist_number : list[int] = []
+
+# for i in range(3,31,3):
+#     print(i)
+
+# ESERCIZIO 4.8
+
+# cubes: list[int]=[]
+# for i in range(1,10):
+#     i=i**3
+#     cubes.append(i)
+# print(cubes)
+
+# ESERCIZIO 4.10
+
+# lista_pizza: list[str] = ["Diavola", "Pistacchiosa", "Margherita","Crostino","Patate","Salsiccia e patate","Marinara","Supplì","Capricciosa"]
+
+# print(f"Questi sono i primi tre elementi della lista {lista_pizza [0:3]}")
+# print(f"Questo è il centro della lista {lista_pizza [3:6]}")
+# print(f"Questa è la fine della tua lista  {lista_pizza [6:9]}")
+
+# ESERCIZIO 4.11
+
+# pizza_list : list[str] = ["Margherita" , "Marinara" , "Diavola"]
+# friend_pizzas:list[str]= ["Margherita" , "Marinara" , "Diavola"]
+# pizza_list.append ("Vegetariana")
+# friend_pizzas.append ("Pistacchiosa")
+
+# for i in pizza_list:
+#     print(f"le mie pizze preferite sono:\n {i}")
+# for i in friend_pizzas:
+#     print(f"Le pizze preferite del mio amico sono:\n {i}")
+
+# ESERCIZIO 4.15
+
+#indentation
+
+# lista_pizza: list[str] = [
+#     "Diavola", "Pistacchiosa", "Margherita"
+#     ]
+# friend_pizzas: list[str] = [
+#     "Diavola", "Pistacchiosa", "Margherita"
+#     ]
+
+# lista_pizza.append ("Vegetariana")
+# friend_pizzas.append ("Napoli")
+
+# for i in lista_pizza:
+#     print (f" Le mie pizze preferite sono: \n {i}")
+# for i in friend_pizzas:
+#     print (f" Le pizze preferite del mio amico sono: \n {i}")
+
+# #comments
+
+# #definisco una lista
+# lista_animali: list[str] = ["foca", "balena", "narvalo", "pesce rosso"]
+
+# #ciclo for
+# for i in lista_animali:
+#     print(i)
+
+# #ciclo for   
+# for i in lista_animali:
+#     print(f" {i} è un animale acquatico")
+
+
+# print("il pesce rosso è un ottimo animale domestico")
+
+# #spaces
+
+# lista_pizza: list[str] = ["Diavola","Pistacchiosa","Margherita"]
+# friend_pizzas: list[str] = ["Diavola","Pistacchiosa","Margherita"]
+
+# lista_pizza.append ("Vegetariana")
+# friend_pizzas.append ("Napoli")
+
+# for i in lista_pizza:
+#     print (f" Le mie pizze preferite sono: \n {i}")
+# for i in friend_pizzas:
+#     print (f" Le pizze preferite del mio amico sono: \n {i}")
+
+
+# ESERCIZIO 5.1
+
+# macchine : str = input("Inserisci il modello della macchina: ")
+
+# predict : bool
+
+# if macchine == "BMW":
+#     predict = True
+#     print(f" is {macchine} == macchina? I predict {predict}")
+
+# elif macchine == "Audi":
+#     predict = True
+#     print(f" is {macchine} == macchina? I predict {predict}")
+
+# elif macchine == "Ferrari":
+#     predict = True
+#     print(f" is {macchine} == macchina? I predict {predict}")
+
+# elif macchine == "Lamborghini":
+#     predict = True
+#     print(f" Is {macchine} == macchina? I predict {predict}")
+
+# elif macchine == "Mercedes":
+#     predict = True
+#     print(f" Is {macchine} == macchin? I predict {predict}")
+
+# elif macchine == "Ducati":
+#     predict = False
+#     print(f" Is {macchine} = macchina? I predict {predict}")
+
+# elif macchine == "SH":
+#     predict = False
+#     print(f" Is {macchine} == macchina? I predict {predict}")
+
+# elif macchine == "KTM":
+#     predict = False
+#     print(f" Is {macchine} == macchiana?  predict {predict}")
+
+# elif macchine == "Kawasaki":
+#     predict = False
+#     print(f" Is {macchine} == macchina? I predict {predict}")
+
+# elif macchine == "Yamaha":
+#     predict = False
+#     print(f" Is {macchine} == macchina. I predict {predict}")
+
+# else:
+#     print(" La macchina inserita non sta nell'elenco ")
+
+# ESERCIZIO 5.2
 
