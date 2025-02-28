dd01f0cdbd079e5df11e486020b66c97b3268736
Mattpwn <argenti.m@hotmail.com>
2025-02-28 19:15:41 +0100

    LEZIONE 3 CICLI

--- Esercizi python/LEZIONE 3 CICLI.py
+++ Esercizi python/LEZIONE 3 CICLI.py
@@ -189,3 +189,189 @@
 
 # ESERCIZIO 5.2
 
+# from typing import Any
+# color:str = input("Inserisci un colore: ")
+# num:int = int(input("Inserisci un numero da 1 a 10: "))
+# list_cose:list[Any] = [1,2,3,4,5,6,7,8,9,10]
+# predict:bool
+
+# if color == "yellow":
+#     predict=True
+#     print(f"Il colore inserito è yellow? {predict}")
+# else:
+#     predict=False
+#     print(f"Il colore inserito è yellow? {predict}")
+
+# if color == "red".lower():
+#     predict=True
+#     print(f"{color.lower()} == color? {predict}")
+# else:
+#     predict=False
+#     print(f"Predict {predict}")
+
+# if num > 6:
+#     predict=True
+#     print(f"Il numero inserito {num} è maggiore di 6? {predict}") 
+# else:
+#     predict=False
+#     print(f"Il numero inserito {num} è maggiore di 6? {predict}")
+
+# if num < 5:
+#     predict=True
+#     print(f"Il numero inserito {num} è minore di 5? {predict}") 
+# else:
+#     predict=False
+#     print(f"Il numero inserito {num} è minore di 5? {predict}")
+
+# if num >= 3:
+#     predict=True
+#     print(f"Il numero inserito {num} è maggiore o uguale a 3? {predict}") 
+# else:
+#     predict=False
+#     print(f"Il numero inserito {num} è maggiore o uguale a 3? {predict}")
+
+# if num <=8:
+#     predict=True
+#     print(f"Il numero inserito {num} è minore o uguale a 8? {predict}") 
+# else:
+#     predict=False
+#     print(f"Il numero inserito {num} è minore o uguale a 8? {predict}")
+
+# if num > 6 or num <3:
+#     predict=True
+#     print(f"Il numero inserito {num} è maggiore di 6 o minore di 3? {predict}") 
+# else:
+#     predict=False
+#     print(f"Il numero inserito {num} è maggiore di 6 o minore di 3? {predict}")
+
+# if num > 2 and num <9:
+#     predict=True
+#     print(f"Il numero inserito {num} è maggire di 2 e minore di 9? {predict}") 
+# else:
+#     predict=False
+#     print(f"Il numero inserito {num} è maggiore di 2 o minore di 9? {predict}")
+
+# if 1 in list_cose:
+#     predict=True
+#     print(f"Il numero 1 è presente nella lista? {predict}")
+# elif 21 not in list_cose:
+#     predict=True
+#     print(f"Il numero 21 non è presente nella lista? {predict}")
+
+
+#ESERCIZIO 5.3
+
+# alien_color:str = "green"
+
+# if alien_color == "green":
+#     print ("the player just earned 5 points")
+
+# alien_color:str = "yellow"
+
+# if alien_color == "green":
+#     print ("the player just earned 5 points")
+
+# #ESERCIZIO 5.4
+
+# alien_color:str = "green"
+
+# if alien_color == "green":
+#     print ("the player just earned 5 points")
+# else:
+#     print ("the player just earned 10 points")
+
+# alien_color:str = "red"
+
+# if alien_color == "green":
+#     print ("the player just earned 5 points")
+# else:
+#     print ("the player just earned 10 points")
+
+# ESERCIZIO 5.5
+
+# alien_color:str = "green"
+# if alien_color == "green":
+#     print("the player earned 5 points")
+# elif alien_color == "yellow":
+#     print("The player earned 10 points")
+# else:
+#     alien_color == "red"
+#     print("The player earned 15 points")
+
+# alien_color:str = "yellow"
+# if alien_color == "yellow":
+#     print("the player earned 5 points")
+# elif alien_color == "green":
+#     print("The player earned 10 points")
+# else:
+#     alien_color == "red"
+#     print("The player earned 15 points")
+
+# alien_color:str = "red"
+# if alien_color == "red":
+#     print("the player earned 5 points")
+# elif alien_color == "yellow":
+#     print("The player earned 10 points")
+# else:
+#     alien_color == "green"
+#     print("The player earned 15 points")
+
+# ESERCIZIO 5.6
+
+# age:int = 21
+
+# if  age < 2:
+#     print(" the person is a baby")
+
+# elif age >= 2 and age < 4:
+#     print("the person is a toddler")
+
+# elif age >= 4 and age < 13:
+#     print("the person is a kid")
+
+# elif age >= 13 and age < 20:
+#     print("the person is a teenager")
+
+# elif age >= 20 and age < 65:
+#     print("the person is an adult")
+
+# else:
+#     print("the person is an elder")
+
+#5.7
+
+# favourite_fruits:list[str] = ["fragola","mela","pesca"]
+
+# if "fragola" in favourite_fruits:
+#     print("You really like Fragola!")
+
+# if "mela" in favourite_fruits:
+#     print("You really like Apples!")
+
+# if "pesca" in favourite_fruits:
+#     print("You really like Peaches!")
+
+# if "banana" in favourite_fruits:
+#     pass
+
+# if "pera" in favourite_fruits:
+#     pass
+
+# ESRCIZIO 5.8
+
+# username:list[str] = ["Admin","Silvia","Luca","Matteo","Thomas"]
+# user:str = input("Log in: ")
+
+# if user=="Admin":
+#     print("Hello Admin, would you like to see a status report?")
+# if user !="Admin":
+#     print(f"Hello {user},thank you for logging in again!")
+
+# ESERCIZIO 5.9
+
+# username:list[str] = []
+
+# if len(username) == 0:
+#     print("We need some Users")
+
+
