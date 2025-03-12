e3833942072d42cad0ecc5daf3563d0c06177c2e
Mattpwn <argenti.m@hotmail.com>
2025-03-12 18:40:07 +0100

    text new clone updated

---
+++ Esercizi python/esercizifunzioniobligatorie.py
@@ -0,0 +1,51 @@
+# ESERCIZIO 8.1
+
+# def display_message (message:str):
+#     print(message)
+
+# display_message ("Tua madre puttana e tu zia nana pelata")
+
+# ESERCIZIO 8.2
+
+# def favorite_book (title:str):
+#     print(f"One of my favorite book is {title}")
+
+# favorite_book ("Meinkauf")
+
+# ESERCIZIO 8.3
+
+# def make_shirt (size:str , message:str):
+#     print(f"le taglie disponibili sono {size} e il messaggio sulla maglia è '{message}' ")
+
+# make_shirt ("XL" , "Vincere e vinceremo")
+
+# ESERCIZIO 8.4
+
+# def make_shirt (size = "L" , message = "I Love Python"):
+#     print(f"le taglie disponibili sono {size} e il messaggio sulla maglia è '{message}' ")
+
+# make_shirt()
+# make_shirt ("M")
+# make_shirt ("XL" , "W Il Duce")
+
+
+# ESERCIZIO 8.5
+
+# def describe_city (city:str , country:str):
+#     print(f" {city} is in {country}")
+
+# describe_city ("Auschwitz" , "Polonia")
+# describe_city ("Pyongyang" , "Corea del Nord")
+# describe_city ("Bagdad" , "Iraq")
+
+# ESERCIZIO 8.6
+
+# def city_country (city:str , country:str):
+#     print(f"{city} , {country} ")
+
+# city_country ("Auschwitz" , "Polonia")
+# city_country ("Pyongyang" , "Corea del Nord")
+# city_country ("Bagdad" , "Iraq")
+
+# ESERCIZIO 8.7
+
