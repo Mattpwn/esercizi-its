a2001f9a14765c4cd804beddf70afab99b5393c7
Mattpwn <argenti.m@hotmail.com>
2025-09-17 11:43:28 +0200

    app web nuova

---
+++ app web/vite-react/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Saluto from './esercizi/Esercizi_1/Saluto'
+import UserAlbumsFullStack from './UserAlbumsFullstack'
+import CardUtente from './esercizi/Esercizio_2/CardUtente'
+import MenuRistorante from './esercizi/Esercizi_3/MenuRistorante'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     <MenuRistorante></MenuRistorante>
+     </>
+  )
+}
+
+export default App
---
+++ app web/vite-react/src/esercizi/piatti.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const piatti =  [
+  { id: 1, nome: "Spaghetti alla Carbonara", prezzo: 12.50 },
+  { id: 2, nome: "Lasagna al Forno", prezzo: 13.00 },
+  { id: 3, nome: "Risotto ai Funghi", prezzo: 11.00 },
+  { id: 4, nome: "Pizza Margherita", prezzo: 9.50 },
+  { id: 5, nome: "Tagliata di Manzo", prezzo: 18.00 },
+  { id: 6, nome: "Insalata Caprese", prezzo: 8.00 },
+  { id: 7, nome: "Tiramisù", prezzo: 6.00 }
+]
+
+export default piatti
