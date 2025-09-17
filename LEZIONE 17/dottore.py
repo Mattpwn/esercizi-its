f5aed068e321bb78baa64fa7d6c1b244e4381969
Mattpwn <argenti.m@hotmail.com>
2025-09-17 19:43:53 +0200

    esercizio classe dottore

--- LEZIONE 17/dottore.py
+++ LEZIONE 17/dottore.py
@@ -1,5 +1,58 @@
 from persona import Persona
 class Dottore(Persona):
 
-    def __init__(self, first_name: str, last_name: str, specialization: str, parcel: float):
-        super().__init__(first_name, last_name)
+    _specializzazione:str
+    _parcella:float
+    
+    def init(self, first_name:str, last_name:str,age:int, specializzazione:str, parcella:float):
+        super().init(first_name,last_name)
+        self.setAge(age)
+        self.setSpecialization(specializzazione)
+        self.setParcel(parcella)
+
+    def setSpecialization(self, specialization):
+        
+        if isinstance(specialization,str):
+            self._specializzazione = specialization
+        
+        else:
+            raise TypeError(f"La specializzazione inserita: {specialization} non è una stringa!")
+
+    def setParcel(self,parcel):
+        
+        if isinstance(parcel,float):
+            self._parcella = parcel
+        
+        
+        else:
+            raise TypeError(f"La parcella inserita: {parcel} non è un float!")
+
+    def getSpecializzazion(self):
+        
+        return f"La specializzaione è {self._specializzazione}"
+
+    def getParcel(self):
+        
+        return f"La parcella del dottore è {self._parcella}"
+
+    def isAValidDoctor(self):
+        
+        if self._age > 30:
+            print(f"Il Dottore {self._first_name} {self._last_name} è un dottore valido")
+        
+        else:
+            print(f"Il Dottore {self._first_name} {self._last_name} non è un dottore valido")
+
+    def doctorGreet(self):
+        Persona.greet(self)
+        print(f"Sono un medico {self.getSpecializzazion()}")
+
+if __name__ == "__main__":
+    dottore:Dottore = Dottore("Matteo","Argenti",22,"Ginecologo",150.3)
+    dottore.doctorGreet()
+    print(dottore.getSpecializzazion())
+    print(dottore.getParcel())
+    dottore.isAValidDoctor()
+
+
+    
