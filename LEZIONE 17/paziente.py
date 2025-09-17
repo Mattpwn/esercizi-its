9a82b1f48d3172463a297b9ee7482eda8b69d314
Mattpwn <argenti.m@hotmail.com>
2025-09-17 19:53:08 +0200

    esercizio classe paziente

---
+++ LEZIONE 17/paziente.py
@@ -0,0 +1,17 @@
+from persona import Persona
+
+class Paziente(Persona):
+    _codice:int
+    def init(self, first_name, last_name,IdCode:int,age:int):
+        super().init(first_name, last_name)
+        self.setAge(age)
+        self.setIdCode(IdCode)
+
+    def setIdCode(self,IdCode:int):
+        self._codice=IdCode
+
+    def getIdCode(self):
+        return self._codice
+
+    def patientInfo(self):
+        print(f"Paziente: {Persona.getName(self)} {Persona.getLastName(self)}\n ID:{self._codice}")
