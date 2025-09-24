b8fb934100872b0a7b0efe5c733a0a4375c224ee
Mattpwn <argenti.m@hotmail.com>
2025-09-24 17:24:10 +0200

    esercizio ToDoList jsx

--- app web/vite-react/src/App.jsx
+++ app web/vite-react/src/App.jsx
@@ -6,6 +6,7 @@ import Saluto from './esercizi/Esercizi_1/Saluto'
 import UserAlbumsFullStack from './UserAlbumsFullstack'
 import CardUtente from './esercizi/Esercizio_2/CardUtente'
 import MenuRistorante from './esercizi/Esercizi_3/MenuRistorante'
+import ToDoApp from './todolist/ToDoApp'
 
 
 
@@ -16,7 +17,8 @@ function App() {
   return (
     <>
      
-     <MenuRistorante></MenuRistorante>
+     <ToDoApp></ToDoApp>
+     
      </>
   )
 }
